a58d75d1ef1d49e6361bca69929bcf881de6d088
Eivind Hammersmark <eivindhammers@gmail.com>
2020-11-24 12:14:02 +0100

    Update deploy.R

--- deploy.R
+++ deploy.R
@@ -1,4 +1,6 @@
+library(rsconnect)
+
 tmp.enc <- options()$encoding
 options(encoding = "UTF-8")
-deployApp(appFiles = c("app.R", "kommuner_2020_simplest.rds", "fylker_2020_simplest.rds"))
+deployApp(appFiles = c("app.R", "kommuner_2020_simplest.rds", "fylker_2020_simplest.rds"), forceUpdate = TRUE)
 options(encoding = tmp.enc)
